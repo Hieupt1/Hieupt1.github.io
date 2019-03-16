07480ef2ca2151f42db7ca350a74f14f75a94f8d
Hieupt1 <hieu.chelsea1@gmail.com>
2019-03-17 05:55:02 +0700

    add

--- Statistics/Kiem_dinh_GT.R
+++ Statistics/Kiem_dinh_GT.R
@@ -1,3 +1,20 @@
+install.packages("BSDA")
+install.packages('stats')
+install.packages('stats4')
+install.packages('utils')
+install.packages('binom')
+install.packages('datasets')
+install.packages('EnvStats')
+install.packages('graphics')
+install.packages('grDevices')
+install.packages('lattice')
+install.packages('methods')
+
+
+library("BSDA")
+
+
+
 ##  BAI 1
 
 #Dung Z.test vi: Da biet phuong sai = 0.08 va tuan theo pp chuan
@@ -40,7 +57,7 @@ mean(x)
 (length(x)-1)*sd(x)^2/length(x)
 
 # UL phuong sai
-varTest(x, conf.level = 0.99)
+varTest(x, conf.level = 0.95)   # add EnvStats library
 
 
 
@@ -60,6 +77,7 @@ L = function(p, x) prod(dbinom(x, size = 1, prob = p))
 
 optimize(L, interval = c(0, 1), x = x, maximum = TRUE)
 
+summary()
 
 ## UL Hop ly Phan phoi chuan
 
@@ -72,7 +90,7 @@ x = PlantGrowth$weight
 # Ham hop ly pp chuan: f(x) = (1/ can(2 *pi) * sigma) * e^(-1.2 * ((x - mu)/sigma)
 minuslogL = function(mu, sigma2) {-sum(dnorm(x, mean = mu, sd =sqrt(sigma2), log = TRUE))}
 
-MaxLikeEst = mle(minuslogL, start = list(mu = 5, sigma2 = 0.5))
+MaxLikeEst = mle(minuslogL, start = list(mu = 5, sigma2 = 0.5)) # add stats4 library
 
 summary(MaxLikeEst)
                  
@@ -117,6 +135,65 @@ z.test(x, sigma.x = 7.6, mu = 178)
 
 zsum.test(x, alt = 'l')
 
+zsum.test(n.x = 16, mean.x = 7.2, mu = 7.6, sigma.x = 1.2, alternative = 'l')
+
+
+## Bai 15
+
+zsum.test(n.x = 20, mean.x = 105, mu = 100, sigma.x = 5, alternative = 'g')
+
+
+## Bai 16
+
+zsum.test(n.x = 2500, mu = 3, mean.x = 2.95, sigma.x = 1, alternative = 'g')
+
+
+## Bai 17
+
+tsum.test(n.x = 25, mean.x = 19.7, mu = 20, s.x = 1.3)
+
+
+## Bai 18
+
+x = c(237, 242, 232, 242, 248, 230, 244, 243, 254, 262, 234, 220, 225, 236, 232, 218, 228, 240)
+
+t.test(x, mu = 240, alternative = 'l')
+
+
+## Bai 19
+
+zsum.test(n.x = 40, mean.x = 0.162, mu = 0.15, sigma.x = 0.04, alternative = 'g', conf.level = 0.99)
+
+
+## Bai 20
+
+x = c(30.1, 32.7, 22.5, 27.5, 27.7, 29.8, 28.9, 31.4, 31.2, 24.3, 26.4, 22.8, 29.1, 33.4, 32.5, 21.7)
+
+shapiro.test(x)
+
+qqnorm(x)
+qqline(x)
+
+t.test(x, mu = 30, alternative = 'l')
+
+
+## Bai 21
+
+#x = data.frame(value = c(19, 20, 21, 22, 23), freq = c(10, 59, 20, 6,5))
+
+x = c( rep(19,10), rep(20, 59), rep(21, 20), rep(22, 6), rep(23, 5) )
+
+t.test(x, mu= 20)
+
+
+## Bai 22
+
+x1 = c(5.5, 6, 7, 6, 7.5, 6, 7.5, 5.5, 7, 6.5, 8.5)
+
+x2 = c(6.5, 6, 8.5, 7, 6.5, 8, 7.5, 6.5, 7.5, 6, 7)
+
+t.test(x1, x2, paired=F, var.equal=F, alternative= 'g')
+
 
 ## Bai 24
 
@@ -126,7 +203,7 @@ x2 = c(3094, 3106, 3004, 3066, 2984, 3124, 3316, 3212, 3380, 3018)
 
 # Day la 2 tong the, lai co cung var => var.equal = T
 
-t.test(x1, x2, var.equal = T)
+t.test(x1, x2, var.equal = T, paired = F)
 
 # Kq: df: bac tu do cua pp T
 
@@ -166,11 +243,129 @@ anova(lm(sanLuong~loaiPhanBon+giongLua))
 TukeyHSD(aov(sanLuong~loaiPhanBon+giongLua))
 
 
+## Bai 2
+
+# a.
+# nhap x theo cot
+x = c(4.05, 4.01, 4.02, 4.04, 3.99, 4.02, 4.01, 3.99, 4.00, 4.00,3.97, 3.98, 3.97, 3.95, 4, 4,  4.00, 4.02, 3.99, 4.01)
+
+phanLoai = c(rep(1, 4), rep(2, 6), rep(3, 6), rep(4, 4))
+
+anova(lm(x~factor(phanLoai)))
+
+TukeyHSD(aov(x~factor(phanLoai)))
+
+# b.
 
 
-anova(lm(sanLuong~loaiPhanBon+giongLua))
+## Bai 3
+
+# nhap x theo hang
+x = c(124.1, 131.5, 127, 126.4, 130.6, 128.4, 127.2, 132.7, 125.6)
+
+xang = gl(3, 3, length=9)
+
+phuGia = gl(3, 1, length= 9)
+
+anova(lm(x~xang+phuGia))
+
+TukeyHSD(aov(x~xang+phuGia))
 
 
+## Bai 4
+
+x = c(121.1, 124.8, 131.5, 131.6, 127, 126,6, 126.4, 127.2, 142.1, 128.4, 142.6, 127.2, 124.9, 132.7, 133.0, 125.6, 120.9)
+
+xang = gl(3, 6, length=18)
+
+phuGia = gl(3, 2, length= 18)
+
+anova(lm(x ~ xang + phuGia))
+
+TukeyHSD(aov(x~xang + phuGia))
+
+
+## Bai 5
+
+chisq.test(x =c(18, 16, 23, 20, 23), p = c(0.2, 0.2, 0.2, 0.2, 0.2))
+
+
+## Bai 6
+
+chisq.test(x= c(458, 30, 12), p = c(0.93, 0.05, 0.02) )
+
+
+## Bai 7
+
+chisq.test(x= c(180, 100, 120), p =c(0.4, 0.3, 0.3))
+
+
+## Bai 8
+
+
+
+## Bai 9
+
+
+
+## Bai10
+
+# nhap theo hang
+x = c(175, 124, 92, 118, 110, 126, 127, 82, 147)
+
+yKien = gl(3, 1, length=9)
+thuNhap = gl(3, 3, length=9)
+
+anova(lm(x~yKien+thuNhap))
+# or
+summary(aov(x~yKien + thuNhap))
+
+TukeyHSD(aov(x~yKien + thuNhap))
+
+
+## Bai11
+
+current_software = c(9.98, 9.98, 9.84, 9.99, 9.94, 9.84, 9.86, 10.12, 9.9, 9.91)
+
+new_software = c(9.88, 9.86, 9.75, 9.8, 9.87, 9.84, 9.87, 9.86, 9.83, 9.86)
+
+# Kiem tra xem co phan phoi chuan khong
+shapiro.test(current_software)
+shapiro.test(new_software)
+
+
+# Do khong co phan phoi chuan
+wilcox.test(current_software, new_software, paired = F)
+
+## Bai12
+
+nam2001 = c(63.1, 67.1, 65.5, 68, 66.6, 65.7, 69.2, 67, 65.2, 60.7)
+nam2002 = c(57.4, 66.4, 68.8, 65.3, 63.5, 66.4, 64.9, 65.2, 65.1, 62.2)
+
+# a. Co phan phoi chuan
+
+t.test(x = nam2001, y = nam2002, paired= F ,var.equal= F, alternative= 'g')
+
+
+# b. Khong co pp chuan
+
+shapiro.test(nam2001)
+shapiro.test(nam2002)
+
+
+qqnorm(nam2001)
+qqline(nam2001)
+
+qqnorm(nam2002)
+qqline(nam2002)
+
+# do la phan phoi chuan
+
+t.test(x = nam2001, y = nam2002, paired= F ,var.equal= F, alternative= 'g')
+
+# neu khong, su dung wilcox.test
+
+wilcox.test(x = nam2001, y= nam2002, paired= F, alt ='g')
 
 
 
@@ -261,43 +456,6 @@ x = c(1, 2, 3, 4,  5, 6, 7, 50)
 boxplot(x)
 
 
-# Bai 1
 
-zsum.test(n.x = 200, mean.x = 935, sigma.x= 180, conf.level = 0.95)
-
-# Bai 2
-
-x = c(120,123,134,123,132,111,176,192,145,133,126,138,
-             123,143,132,153,143,164,174,184,142,165,102,123,
-             112,127,156,176,145,204,275,284,195,143,134,127,
-             119,134,245,256,364,373,367,283,293,274,246,234)
-
-# Phan loai theo cua hang
-cuaHang = gl(4,12, length = 48)
-
-# Phan loai theo thang
-dtThang = gl(12,1, length = 48)
-
-anova(lm(x~cuaHang+dtThang))
-
-# Tukey anova: Di sau hon vao tung nhom
-TukeyHSD(aov(x~cuaHang+dtThang))
-
-
-# Bai 4
-
-x = c(9.98, 9.98, 9.84, 9.99, 9.94, 9.94, 9.86, 10.12, 9.9, 9.91, 9.88, 9.86, 9.75, 9.8, 9.87, 9.84, 9.87, 9.86, 9.83, 9.86)
-
-
-# phan loai theo cua hang
-phienBan = gl(2,10, length = 20)
-
-# phan loai theo Lenh
-lenh = gl(10, 1, length = 20)
-
-anova(lm(x~phienBan+lenh))
-
-
-# Bai 5
 
 
